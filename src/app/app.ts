dfa37d850d302a04d0f06e14179bd1cb32cf5bd7
spadave7 <spadave7@gmail.com>
2026-01-09 13:32:09 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Technical_Test_Newel');
+}
